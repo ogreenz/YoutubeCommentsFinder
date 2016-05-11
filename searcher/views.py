0a8cd273aeebe4c73ab6f9fa9297374d9fd6c2c0
OfirG <a>
2016-05-11 15:51:04 +0300

    created the searching view scheme (broken code) that sketches the work that need to be done.

--- searcher/views.py
+++ searcher/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def searcher(request, video_name, uploader_name):
+    rows = DB.getVideosAndComments(video_name, uplaoder_name)
+    context = {'rows': rows}
+    if rows:
+        return render(request, 'searcher/index.html', context)
+    else:
+        # lets fetch some information from youtube
+        addvideotointernalDB
+        rows = DB.getVideosAndComments(...)
+        if not rows:
+            return HttpResponse("we got nothing")
+        return render(request, 'searcher/index.html', context)
