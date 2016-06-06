15fe4b1a384046d5c7df64154c345a2fdcddcddc
macDemarco <shevach.tamar@gmail.com>
2016-06-06 18:03:16 +0300

    Removed db stuff
    since we're no longer using the django db

--- searcher/admin.py
+++ searcher/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import Videos, Users, Comments
-
-admin.site.register(Videos)
-admin.site.register(Users)
-admin.site.register(Comments)
-# Register your models here.
